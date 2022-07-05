da719ac11f04c99d36b2282dd389c3347de4d9cc
perezlira <robertocpl94@gmail.com>
2022-07-04 23:21:46 -0600

    agregando video local

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,8 @@
 <div class="container-fluid bg-light.bg-gradient" id="about">
         <div class="row">
           <div class="col-md-6 shadow-lg p-3 mb-5 bg-body rounded">
-          <video class="section d-flex justify-content-center embed-responsive embed-responsive-21by9" autoplay loop muted>
-        <source src="https://mdbcdn.b-cdn.net/img/video/Tropical.mp4" type="video/mp4" />
+              <video class="img-fluid" autoplay loop muted>
+        <source src="{{url('/images/video.mp4')}}" type="video/mp4" />
       </video>
     
     
