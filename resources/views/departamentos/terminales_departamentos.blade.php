41c9634adaa2c526c07377954dbc30ea690e0aa7
perezlira <robertocpl94@gmail.com>
2022-07-12 23:32:26 -0600

    agregando datatable

--- resources/views/departamentos/terminales_departamentos.blade.php
+++ resources/views/departamentos/terminales_departamentos.blade.php
@@ -1,52 +1,38 @@
 @extends('layouts.plantilla')
 @section('title','Terminales')
-
 @section('content')
-@if(isset($autobuses))
-    
-        <div class="card">
-            <div class="card-header">
 
-                    <h3 >{{$terminal->nombre}}</h3>
-            </div>
-        </div>
-  
+ <div class="card">
+    <div class="card-header">
 
-    @foreach($autobuses as $autobus)
-        <div class="card">
-            <div class="card-header">
-                <table class="table table-dark table-striped-columns">
-                    <thead>
-                        <tr>
-                            <th>Nombre del bus</th>
-                            <th>Placa</th>
-                            <th>Sale a:</th>
-                            <th>Hora de salida:</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{{$autobus->nombre}}</td>
-                            <td>{{$autobus->placa}}</td>
-                            <td>{{$autobus->destino}}</td>
-                            <td>{{$autobus->hora_salida}}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
-    @endforeach
-@else
-    @foreach($terminales as $terminal)
-    <div class="card">
-        <div class="card-header">
-            <a href="{{route('departamento.autobuses',$terminal)}}">
-                <h3 {{$terminal->id}}>{{$terminal->nombre}}</h3>
-            </a>
-        </div>
+        <h3 >{{$terminal->nombre}}</h3>
+     </div>
+</div>
+ <div class="card">
+    <div class="card-body">
+        <table class="table table-dark table-striped-columns" id="buses">
+            <thead>
+                <tr>
+                    <th>Nombre del bus</th>
+                    <th>Placa</th>
+                    <th>Sale a:</th>
+                    <th>Hora de salida:</th>
+                </tr>
+            </thead>
+@foreach($autobuses as $autobus)
+            <tbody>
+                <tr>
+                    <td>{{$autobus->nombre}}</td>
+                    <td>{{$autobus->placa}}</td>
+                    <td>{{$autobus->destino}}</td>
+                    <td>{{$autobus->hora_salida}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-    @endforeach
-        
- @endif
+ </div>
+
+ 
+  
 @endsection
