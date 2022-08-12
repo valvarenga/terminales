4597d970283dfe7d5ba9b2ba3f85a72ab2bb7c7b
valvarenga4 <valvarenga4@gmail.com>
2022-08-12 16:42:28 -0600

    Mejora #2 en el cuadro de busqueda

--- app/Http/Controllers/BuscarController.php
+++ app/Http/Controllers/BuscarController.php
@@ -6,15 +6,37 @@ use App\Model\Post;
 use Illuminate\Support\Facades\DB;
 use App\Models\Terminales;
 use App\Models\Municipios;
+use App\Models\Autobuses;
+use League\CommonMark\Normalizer\SlugNormalizer;
 
 class BuscarController extends Controller
 {
     public function index(Request $request){
         
        $municipio_id=$request->get('id_municipio');
-        $municipio= new Municipios();
         $terminales=Terminales::all()->where('municipio_id',$municipio_id);
-        return view('buscar.buscar', compact('terminales',));
-    }
+        if(count($terminales)>=2){
+            return view('buscar.buscar', compact('terminales',));
+        }
+        elseif(count($terminales)==1){
+            
+            $autobus=Autobuses::all()->where('id',$terminales);
+            $terminal=$terminales;
+            json_decode($terminal);
+            foreach($terminal as $ter){
+                $nombre= $ter->slug;
+                return redirect(route('departamento.autobuses', $ter));
+            }
+        }
+            else{
+                echo "No hay terminales";
+            }
+            
+           // return route(route('departamento.autobuses', $terminal));
+            //view('departamentos.terminales_departamentos', compact('autobuses','terminal'));
+                
+        }        
+        
+    
 
 }
