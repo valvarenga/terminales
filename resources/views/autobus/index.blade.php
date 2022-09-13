627201123f7dfeb6ad3b736aa0946784b7e69830
perezlira <robertocpl94@gmail.com>
2022-09-12 20:56:32 -0600

    estlo ala  vista autobus

--- resources/views/autobus/index.blade.php
+++ resources/views/autobus/index.blade.php
@@ -1,85 +1,85 @@
 @extends('layouts.plantilla')
 @section('title', 'Nuevo Autobus')
-
 @section('content')
 
+<div class="container">
+    <div class="row">
+<div class="card">
+    <div class="card-header text-center">
+        <h3 >Horarios</h3>
+    </div>
+</div>
+
+<div class="card-body">
 <form action="{{route('autobus')}}" method="POST">
     @csrf
-    <div>
-        <label for="terminal">
-            <br>
-            <select name="terminal" id="terminal">
+<div class="form-group">
+        <label for="terminal" class="h4">Terminal </label>
+            <select name="terminal" id="terminal" class="form-select" aria-label="Default select example">
                 <option value="">Seleccione una opción</option>
                 @foreach ($terminales as $terminal)
                 <option value="{{$terminal->id}}">{{$terminal->nombre}}</option>
                 @endforeach
             </select>
-        </label>
+        
     </div>
     @error('terminal')
     <div class="alert alert-danger">{{ $message }}</div>
     @enderror
+     <br/> 
 
-    <div>
-        <label for="nombre">Nombre del autobus
-            <br>
-            <input type="text" name="nombre" id="nombre" value="{{old('nombre')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Nombre aquí">
-        </label>
-    </div>
-    @error('nombre')
+ <div class="form-group">
+     <label for="nombre" class="h4 ">Nombre del Autobus</label>
+        <input type="text" name="nombre" id="nombre" class="form-control" value="{{old('nombre')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Nombre aquí">
+    </div>   
+ @error('nombre')
     <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+@enderror
+   <br/>
 
-    <div>
-        <label for="placa">Placa del autobus
-            <br>
-            <input type="text" name="placa" id="placa" value="{{old('placa')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Placa aquí">
-        </label>
-    </div>
-    
-
-    <div>
-        <label for="origen"> Sale de: <br>
-            <input type="text" name="origen" id="origen" value="{{old('origen')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Ingrese el origen del bus">
-        </label>
-    </div>
+<div class="form-group"> 
+     <label for="placa" class="h4">Placa del autobus</label>
+     <input type="text" name="placa" id="placa" class="form-control" value="{{old('placa')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Placa aquí">
+</div>
+    <br/>
+ <div class="form-group"> 
+    <label for="origen" class="h4"> Sale de:  </label>
+     <input type="text" name="origen" id="origen"  class="form-control" value="{{old('origen')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Ingrese el origen del bus">
+ </div>
     @error('origen')
     <div class="alert alert-danger">{{ $message }}</div>
     @enderror
-
-    <div>
-        <label for="hora_llegada">Hora de salida
-            <br>
-            <input type="time" name="hora_salida" id="hora_salida" value="{{old('hora_salida')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Hora de llegada aquí">
-        </label>
-    </div>
+   <br/>
+<div class="form-group"> 
+        <label for="hora_llegada" class="h4">Hora de salida</label>
+            <input type="time" name="hora_salida" id="hora_salida"  class="form-control" value="{{old('hora_salida')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Hora de llegada aquí">
+ </div>
     @error('hora_salida')
     <div class="alert alert-danger">{{ $message }}</div>    
     @enderror
-
-    <div>
-        <label for="destino">Llega a:
-            <br>
-            <input type="text" name="destino" id="destino" value="{{old('destino')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Ingrese el destino">
-        </label>
-    </div>
+<br/>
+<div class="form-group"> 
+    <label for="destino" class="h4">Llega a:</label>
+    <input type="text" name="destino" id="destino"  class="form-control" value="{{old('destino')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Ingrese el destino">
+ </div>
     @error('destino')
     <div class="alert alert-danger">{{ $message }}</div>
     @enderror
-
-    <div>
-        <label for="hora_llegada">Hora de llegada
-            <br>
-            <input type="time" name="hora_llegada" id="hora_llegada" value="{{old('hora_llegada')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Hora de llegada aquí">
-        </label>
-    </div>
+ <br/>
+ <div class="form-group"> 
+    <label for="hora_llegada" class="h4">Hora de llegada</label>
+    <input type="time" name="hora_llegada" id="hora_llegada"  class="form-control" value="{{old('hora_llegada')}}" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Hora de llegada aquí">
+</div>
     @error('hora_llegada')
     <div class="alert alert-danger">{{ $message }}</div>
     @enderror
-
-    <br>
-    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-        Guardar
-    </button>
+<br/>
+    
+<button type="submit" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-ok"></span>
+ Guardar
+</button>
 </form>
+</div>
+    </div>
+    </div>
 @endsection
