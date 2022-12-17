a258107ed0bc7e7dd3a449348e97a05b25044bef
perezlira <robertocpl94@gmail.com>
2022-12-16 21:54:51 -0600

    actualizar informacion

--- resources/views/enlaces/acerca.blade.php
+++ resources/views/enlaces/acerca.blade.php
@@ -5,20 +5,30 @@
 @section('content')
 <div class="container-sm bg-light.bg-gradient fst-italic fs-3" id="anun">
 <div class="row">
-<h1 class="display-4 text-center text-success">Conoce sobre este proyecto, Estelí Buse</h1>
+<h1 class="display-4 text-center text-success">Conoce sobre este proyecto, NicaBuses</h1>
 <p>
-Estelí Buses es un proyecto personal y totalmente voluntario diseñado, desarrollado y mantenido completamente por el desarrollador web Félix Icaza, 
+NicaBuses es un proyecto personal y totalmente voluntario diseñado, desarrollado y mantenido completamente por los  
+desarrolladores web Victor Alvarenga, Roberto Perez Lira
+y Nestor Lozano , 
 quién no está asociado ni relacionado con ninguna cooperativa, 
-ni con ningún transportista y tampoco con las terminales de buses de Estelí.</p>
+ni con ningún transportista y tampoco con las terminales de buses de Nicaragua.</p>
 <h2 class="text-success fw-bolder">¿Porqué existe este proyecto</h2>
 <p class="fst-italic fs-3">
-A lo largo de los años, los Estelianos siempre hemos tenido la dificultad de conocer con exactitud la hora de salida de los buses de la terminal norte y de la terminal sur. La única forma es llamando por teléfono directamente a las terminales, pero también algunas personas no tienen el acceso a conocer el número de teléfono de cada terminal, inclusive tener que ir personalmente a las terminales para solicitar dicha información.
+A lo largo de los años, los Nicaraguenses siempre hemos tenido la dificultad de conocer con exactitud la hora 
+de salida de los buses de las distintas terminales. La única forma es llamando por teléfono directamente a las terminales,
+ pero también algunas personas no tienen el acceso a conocer el número de teléfono de cada terminal, inclusive tener que ir
+  personalmente a las terminales para solicitar dicha información.
 
-Así que decidí crear este proyecto, con la idea principal para intentar de solventar esa problemática o dificultad que existe en relación a los horarios de las terminales de buses, así, todas las personas que recidan en Estelí o cualquier turista que desee viajar desde Estelí hacia el interior del país, puedan conocer fácilmente los horarios y todos los destinos de dichas terminales y esté accesible para todos.
+Así que decidimos crear este proyecto, con la idea principal para intentar de solventar esa problemática 
+o dificultad que existe en relación a los horarios de las terminales de buses, así, todas las personas 
+que recidan en Nicaragua o cualquier turista que desee viajar desde cualquier parte del país,
+ puedan conocer fácilmente los horarios y todos los destinos de dichas terminales y esté accesible para todos.
 
-Ya que de ésta forma cualquier información que la población de Estelí necesite estará a su alcance y hará mucho más fácil, conocer la información relacionada a cada terminal de buses.
+Ya que de ésta forma cualquier información que la población de Nicaragua necesite estará a su alcance y hará mucho más fácil, 
+conocer la información relacionada a cada terminal de buses.
 
-Es por eso que es importante, que al conocer este proyecto lo compartas con todos tus familiares, amigos y conocidos, para así ayudar a más personas.
+Es por eso que es importante, que al conocer este proyecto lo compartas con todos tus familiares, amigos y conocidos, 
+para así ayudar a más personas.
 </p>
 
 
